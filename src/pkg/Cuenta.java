be5264b7d22a090556158148bac61ba099811508
usuario <usuario@pc20-013>
2023-10-10 11:02:16 +0200

    refactorizacion del codigo

--- src/pkg/Cuenta.java
+++ src/pkg/Cuenta.java
@@ -1,6 +1,7 @@
 package pkg;
 
 public class Cuenta {
+
 	String numero;
 	String titular;
 	double saldo;
@@ -11,11 +12,22 @@ public class Cuenta {
 		this.titular = titular;
 		this.saldo = saldo;
 	}
-
-	public void ingresar(int i) {
-		saldo += i;
+	public String getNumero() {
+		return numero;
 	}
 
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+	
 	public void setSaldo(double saldo) {
 		this.saldo =  saldo; 
 	}
@@ -24,6 +36,11 @@ public class Cuenta {
 		return saldo;
 	}
 
+	public void ingresar(int i) {
+		this.setSaldo(this.getSaldo() + i);
+	}
+
+	
 	public void retirar(int i) {
 		double res = this.saldo - i;
 		if (res >= 0) {
