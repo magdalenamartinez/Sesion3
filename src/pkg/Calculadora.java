9bbb7455538a273dee401de02c427f57cdf5ca01
usuario <usuario@pc20-013>
2023-10-10 10:17:01 +0200

    TDD PASO 1, CREAR PRUEBA

---
+++ src/pkg/Calculadora.java
@@ -0,0 +1,15 @@
+package pkg;
+
+public class Calculadora {
+
+	public static Integer suma(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Integer resta(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
